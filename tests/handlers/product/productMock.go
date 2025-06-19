eea803aba58864b6ea7616af7b312e5c413ece60
go-mod-test <maingofer@mail.ru>
2025-06-19 16:58:17 +0300

    work with tests

---
+++ tests/handlers/product/productMock.go
@@ -0,0 +1,33 @@
+package product_test
+
+import (
+	"context"
+
+	"github.com/go-mod-test/goods/internal/domain"
+)
+
+type mockCustomerGetter struct {
+	mockData    []domain.Customer
+	oneMockData domain.Customer
+	err         error
+}
+
+func (m *mockCustomerGetter) GetAllCustomers(_ context.Context) ([]domain.Customer, error) {
+	return m.mockData, m.err
+}
+
+func (m *mockCustomerGetter) GetOneCustomer(_ context.Context, _ int) (domain.Customer, error) {
+	return m.oneMockData, m.err
+}
+
+func (m *mockCustomerGetter) CreateCustomer(_ context.Context, _ domain.Customer) error {
+	return m.err
+}
+
+func (m *mockCustomerGetter) UpdateCustomer(_ context.Context, _ domain.Customer) error {
+	return m.err
+}
+
+func (m *mockCustomerGetter) DelOneCustomer(_ context.Context, _ int) error {
+	return m.err
+}
